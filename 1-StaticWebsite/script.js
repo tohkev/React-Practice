6ae036f5aced02db89d961bdd7a7cc8f45171253
Kevin <tohkev@gmail.com>
2022-01-08 11:54:19 -0800

    Divided code into components, added styles

--- 1-StaticWebsite/script.js
+++ 1-StaticWebsite/script.js
@@ -2,22 +2,48 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
+function Header() {
+    return (
+        <nav className="nav-bar">
+            <img src="./img/react-logo.png" alt="React Logo" className="nav-logo" />
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+function MainContent() {
+    return (
+        <main className="main-content">
+            <h1 className="main-header">Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <small>© 2021 Sample development. All rights reserved.</small>
+        </footer>
+    )
+}
+
 function StaticWebpage() {
     return (
         <div>
-            <nav>
-                <img src="./img/react-logo.png" width="40px" alt="React Logo" />
-            </nav>
-            <main>
-                <h1>Fun facts about React</h1>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 100K stars on Github</li>
-                    <li>Is maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </main>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
